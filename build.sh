2cfffc599de58710ca6ebfcb781635e6f0d540ac
MartinHouhui <robinshouhui@gmail.com>
2017-04-26 00:23:31 +0800

    add

--- build.sh
+++ build.sh
@@ -7,14 +7,23 @@ if [ "${DOMAIN}" == "**None**" ]; then
 fi
 
 cd ${MY_FILES}
-if [ ! -f "${MY_FILES}/base.pem" ]; then
-    openssl genrsa -out base.key 2048
-    openssl req -new -x509 -nodes -key base.key -days 10000 -subj "/CN=${DOMAIN}" -out base.pem
+if [ ! -f "${MY_FILES}/rootCA.pem" ]; then
+    openssl genrsa -out rootCA.key 2048
+
+    openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+
     openssl genrsa -out device.key 2048
-    openssl req -new -key device.key -subj "/CN=${DOMAIN}" -out device.csr
-    openssl x509 -req -in device.csr -CA base.pem -CAkey base.key -CAcreateserial -days 10000 -out device.crt
+
+    openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
+
+    openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+
+    cp rootCA.pem assets/client/tls/ngrokroot.crt
+
+    cp device.crt assets/server/tls/snakeoil.crt
+
+    cp device.key assets/server/tls/snakeoil.key
 fi
-cp -r base.pem /ngrok/assets/client/tls/ngrokroot.crt
 
 cp -r /ngrok/bin ${MY_FILES}/bin
 
