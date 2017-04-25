11c151efcce339cd5b4a8f2bccd68c8b555480a4
MartinHouhui <robinshouhui@gmail.com>
2017-04-25 23:57:44 +0800

    add

--- build.sh
+++ build.sh
@@ -7,17 +7,6 @@ if [ "${DOMAIN}" == "**None**" ]; then
 fi
 
 cd ${MY_FILES}
-if [ ! -f "${MY_FILES}/rootCA.pem" ]; then
-    openssl genrsa -out rootCA.key 2048
-    openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
-    openssl genrsa -out server.key 2048
-    openssl req -new -key server.key -subj "/CN=$NGROK_DOMAIN" -out server.csr
-    openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 5000
-fi
-
-cp -r rootCA.pem /ngrok/assets/client/tls/ngrokroot.crt
-cp -r server.crt /ngrok/assets/server/tls/snakeoil.crt
-cp -r server.key /ngrok/assets/server/tls/snakeoil.key
 
 cd /ngrok
 GOOS=linux GOARCH=amd64 make release-server 
