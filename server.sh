6c385dc5103018767f34bdfb67cccea0071abefc
MartinHouhui <robinshouhui@gmail.com>
2017-04-26 00:29:11 +0800

    add

--- server.sh
+++ server.sh
@@ -11,5 +11,4 @@ if [ ! -f "${MY_FILES}/bin/ngrokd" ]; then
     /bin/sh /build.sh
 fi
 
-
-${MY_FILES}/bin/ngrokd -domain="${DOMAIN}" -httpAddr=${HTTP_ADDR} -httpsAddr=${HTTPS_ADDR} -tunnelAddr=${TUNNEL_ADDR}
+${MY_FILES}/bin/ngrokd -tlsKey=${MY_FILES}/device.key -tlsCrt=${MY_FILES}/device.crt -domain="${DOMAIN}" -httpAddr=${HTTP_ADDR} -httpsAddr=${HTTPS_ADDR} -tunnelAddr=${TUNNEL_ADDR}
